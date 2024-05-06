673bbe70159f1cec6a926b77a5f384220023b7e4
BaSO-4 <luka.sabotic@gmail.com>
2024-05-06 11:51:34 +0200

    functions init

--- sat_solver.py
+++ sat_solver.py
@@ -4,5 +4,17 @@ class Solver:
     def __init__(self) -> None:
         pass
 
+    def solve(self, formula):
+        pass
+
+    def simplify(self, formula):
+        pass
+
+def read_input(path):
+    pass
+
+def write_output(path, solution):
+    pass
+
 if __name__ == "__main__":
     pass
